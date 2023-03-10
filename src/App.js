b3d64041e24ef3c2d90369ec0e64563ff9fb021f
sbrb <info.sbriju@gmail.com>
2023-03-10 13:51:55 +0530

    fg

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
     <Router>
       <Switch>
         <Route path="/" exact>
-          <Redirect to={`/documents/${uuidV4()}`} />
+          <Redirect to={`/${uuidV4()}`} />
         </Route>
-        <Route path="/documents/:id">
+        <Route path="/:id">
           <TextEditor />
         </Route>
       </Switch>
