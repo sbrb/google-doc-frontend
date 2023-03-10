3ce48a2fb9dac8451ebc39e7af571d2f98035a07
sbrb <info.sbriju@gmail.com>
2023-03-10 10:43:36 +0530

    3

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -23,7 +23,7 @@ export default function TextEditor() {
   const [quill, setQuill] = useState()
 
   useEffect(() => {
-    const s = io("http://localhost:3001")
+    const s = io("https://google-doc-backend.vercel.app")
     setSocket(s)
 
     return () => {
